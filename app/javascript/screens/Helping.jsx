e87ae06cfe6e28e9b5e3c57ea2e03985f89d4cb4
Marcus <mboperator@gmail.com>
2017-12-09 20:57:07 -0800

    Filling out I want to Help

--- app/javascript/screens/Helping.jsx
+++ app/javascript/screens/Helping.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import Layout from '../components/Layout'
 
 const Container = styled.div`
   display: flex;
@@ -11,10 +12,7 @@ const HeaderContainer = styled.div`
   align-self: flex-start;
   flex: 1;
   text-transform: uppercase;
-`
-
-const NavContainer = styled.nav`
-  flex: 4;
+  font-size: 2.25rem;
 `
 
 const NavigationCard = styled.section`
@@ -25,44 +23,69 @@ const NavigationCard = styled.section`
   text-align: center;
 `
 
-const TranslateLink = styled.aside`
-  display: block;
-  text-align: center;
-  padding: 35px 15px;
+const Category = styled(Link)`
+  color: #000;
+  font-size: 2rem;
+  padding: 25px 25px;
+  text-decoration: none;
+  margin-bottom: 1rem;
+
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2), 0 1px 3px 0 rgba(0, 0, 0, 0.1);
+  transition: box-shadow 0.2s ease-in-out;
+  &:hover {
+    box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.23), 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+  }
+`
+
+const External = styled.a`
+  color: #000;
+  font-size: 2rem;
+  padding: 25px 25px;
+  text-decoration: none;
+  margin-bottom: 1rem;
+
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2), 0 1px 3px 0 rgba(0, 0, 0, 0.1);
+  transition: box-shadow 0.2s ease-in-out;
+  &:hover {
+    box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.23), 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+  }
 `
 
 const Footer = styled.footer`
   text-align: center;
 `
 
-const Home = props => (
-  <Container>
-    <HeaderContainer>
-      <h1>
-        Thomas Fire Help
-      </h1>
-    </HeaderContainer>
-    <NavContainer>
-      <Link to="/">
-        <NavigationCard>
-          Water/Food
-        </NavigationCard>
-      </Link>
-      <Link to="/">
-        <NavigationCard>
-          Shelter
-        </NavigationCard>
-      </Link>
-    </NavContainer>
-
-    <TranslateLink>
-      en Español
-    </TranslateLink>
-
-    <Footer>
-      In event of Emergency, call 911
-    </Footer>
-  </Container>
+const Home = ({ history: { goBack }}) => (
+  <Layout onBack={goBack}>
+    <Container>
+      <HeaderContainer>
+        <h1>
+          HOW CAN YOU HELP?
+        </h1>
+      </HeaderContainer>
+      <External
+        href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=765665098"
+        target="_blank"
+      >
+        I can provide food or water
+      </External>
+      <External
+        href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=427392080"
+        target="_blank"
+      >
+        I can provide supplies
+      </External>
+      <Category to="/looking_for_help/housing">
+        I can provide housing
+      </Category>
+      <External
+        href="https://docs.google.com/spreadsheets/d/1miFPjSVhG8NR-tpieEH3Tu9cusCAesCSfr6ro1Nbd88/edit#gid=855993057"
+        target="_blank"
+      >
+        I can provide animal services
+      </External>
+    </Container>
+  </Layout>
 )
 
 export default Home
