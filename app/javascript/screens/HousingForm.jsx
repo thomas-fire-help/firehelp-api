fa66ea89815ef97362a74bddff13c1faecb67a10
Ian Nicholson <ipnicholson@gmail.com>
2017-12-09 22:53:26 -0800

    25% complete - heading home for night

--- app/javascript/screens/HousingForm.jsx
+++ app/javascript/screens/HousingForm.jsx
@@ -1,39 +1,67 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import { Input } from 'antd';
+import { Input, Radio, Checkbox} from 'antd';
+
+const RadioGroup = Radio.Group;
+const { TextArea } = Input;
 
 const Housing = ({ data, history: { goBack }}) => (
   <Layout header="Housing" onBack={goBack}>
     <Input placeholder="Number of beds" />
     <Input placeholder="City" />
     <div>
-      <label>Length Available</label>
+      <label>Length Available</label>  
+      <RadioGroup>
+        <Radio value={1}>Short-Term: one week or less</Radio>
+        <Radio value={2}>Long-Term: one month to one week </Radio>
+        <Radio value={3}>Permanent: available for rent or lease</Radio>
+      </RadioGroup>
     </div>
 
     <div>
-      <input placeholder="Child Friendly" />
+      <Checkbox>
+        Child Friendly
+      </Checkbox>
     </div>
 
     <div>
-      <input placeholder="Notes about Kids" />
+      <TextArea placeholder="Notes About Kids" autosize={{ minRows: 2 }} />
     </div>
 
     <div>
-      <input
-        placeholder="Pets accepted?"
-      />
+      <h2>Pets</h2>
+      <Checkbox>
+        No Pets Allowed // Make controlled checkbox that disables below choices
+      </Checkbox>
+      <Checkbox>
+        Large Dogs (more than 50 lbs.)
+      </Checkbox>
+      <Checkbox>
+        Small Dogs (less than 50 lbs.)
+      </Checkbox>
+      <Checkbox>
+        Cats
+      </Checkbox>
+      <Checkbox>
+        Horses
+      </Checkbox>
+      <Checkbox>
+        Birds
+      </Checkbox>
+      <Checkbox>
+        Reptiles
+      </Checkbox>
+      <Checkbox>
+        Rodents (i.e. Rabbits, Hamsters, Gerbils)
+      </Checkbox>
     </div>
 
     <div>
-      <input
-        placeholder="Pet restrictions & additional information (e.g. - large yard, dogs must be kid-friendly, indoor cats only, etc.)"
-      />
+      <TextArea placeholder="Pet restrictions & additional information (e.g. - large yard, dogs must be kid-friendly, indoor cats only, etc.)" autosize={{ minRows: 2 }} />
     </div>
 
     <div>
-      <input
-        placeholder="Additional Information"
-      />
+      <TextArea placeholder="Additional Information" autosize={{ minRows: 2 }} />
     </div>
   </Layout>
 )
