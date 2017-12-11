a87ecd6bdbda4ed3f7e85a4ada421ffddbf28876
Ian Nicholson <ipnicholson@gmail.com>
2017-12-10 20:17:28 -0800

    add responsive

--- app/javascript/screens/HousingForm.jsx
+++ app/javascript/screens/HousingForm.jsx
@@ -48,6 +48,12 @@ background-color: #f7f7f7;
 text-align: center;
 padding: 1rem;
 `
+const FormContainer = styled.div`
+min-width: 200px;
+max-width: 768px;
+flex-basis: auto; /* default value */
+flex-grow: 1;
+`
 
 const ItemDiv = styled.div`
 display: flex;
@@ -77,110 +83,113 @@ function onChangeHousingType(value) {
 
 const HousingForm = ({ actions, formData, history: { goBack }}) => (
   <Layout header="HousingForm" onBack={goBack}>
-    <PageHeading>
-      <h3>I Have Housing To Offer</h3>
-    </PageHeading>
+    <FormContainer>
+      <PageHeading>
+        <h3>I Have Housing To Offer</h3>
+      </PageHeading>
 
-    <ItemDiv>
-      <label>Housing Type:</label>
-      <RadioGroup onChange={onChangeHousingType} defaultValue="a" value={housingType.state.value}>
-        <RadioButton value="a">Entire Home</RadioButton>
-        <RadioButton value="b">Private Room</RadioButton>
-      </RadioGroup>
+      <ItemDiv>
+        <label>Housing Type:</label>
+        <RadioGroup onChange={onChangeHousingType} defaultValue="a" value={housingType.state.value}>
+          <RadioButton value="a">Entire Home</RadioButton>
+          <RadioButton value="b">Private Room</RadioButton>
+        </RadioGroup>
+        
+      </ItemDiv>
       
+      <ItemDiv>
+        <label>Beds Available:</label>
+        <InputNumber min={1} max={40} /*style={this.defaultProps.styles.inputNumber}*/ />
+      </ItemDiv>
+      <ItemDiv>
+        City:
+        <Input />
+      </ItemDiv>
+      <ItemDiv>
+        Neighborhood:
+        <Input />
+      </ItemDiv>
+      <ItemDiv>
+        <label>Length Available:</label>  
+        <Cascader options = {durationOptions} onChange = {onChangeDuration} placeholder="Select" />
+      </ItemDiv>
+
+      <ItemDiv>
+        Child Friendly:
+        <RadioGroup value={childFriendly.state.value}>
+          <Radio value={1}>Yes</Radio>
+          <Radio value={2}>No</Radio>
+        </RadioGroup>
+      </ItemDiv>
+
+      <ItemDiv>
+        Notes About Children:
+        <TextArea autosize={{ minRows: 2 }} />
+      </ItemDiv>
+
+      <ItemDiv>
+        Household currently has animals?:
+        <RadioGroup value={animalsPresent.state.value}>
+          <Radio value={1}>Yes</Radio>
+          <Radio value={2}>No</Radio>
+        </RadioGroup>
+        What type(s)?:
+        <TextArea />
+      </ItemDiv>
+
+      <ItemDiv>
+        Pets
+        <Checkbox>
+          No Pets Allowed // Make controlled checkbox that disables below choices
+        </Checkbox>
+        <Checkbox>
+          Large Dogs (more than 50 lbs.)
+        </Checkbox>
+        <Checkbox>
+          Small Dogs (less than 50 lbs.)
+        </Checkbox>
+        <Checkbox>
+          Cats
+        </Checkbox>
+        <Checkbox>
+          Horses
+        </Checkbox>
+        <Checkbox>
+          Birds
+        </Checkbox>
+        <Checkbox>
+          Reptiles
+        </Checkbox>
+        <Checkbox>
+          Rodents (i.e. Rabbits, Hamsters, Gerbils)
+        </Checkbox>
+      </ItemDiv>
+
+      <ItemDiv>
+        Notes About Pets:
+        <TextArea autosize={{ minRows: 2 }} />
+      </ItemDiv>
+
+      <ItemDiv>
+        Photo(s) of Housing:
+        <Upload {...housingPhotoListProps}>
+          <Button>
+            <Icon type="upload" /> upload
+          </Button>
+        </Upload>
+        <br />
     </ItemDiv>
+
+      <ItemDiv>
+        Additional Information
+        <TextArea placeholder="Additional Information" autosize={{ minRows: 2 }} />
+      </ItemDiv>
+
+      <button onClick={() => actions.create(formData)}>
+        Submit!
+      </button>  
+    </FormContainer>
     
-    <ItemDiv>
-      <label>Beds Available:</label>
-      <InputNumber min={1} max={40} /*style={this.defaultProps.styles.inputNumber}*/ />
-    </ItemDiv>
-    <ItemDiv>
-      City:
-      <Input />
-    </ItemDiv>
-    <ItemDiv>
-      Neighborhood:
-      <Input />
-    </ItemDiv>
-    <ItemDiv>
-      <label>Length Available:</label>  
-      <Cascader options = {durationOptions} onChange = {onChangeDuration} placeholder="Select" />
-    </ItemDiv>
-
-    <ItemDiv>
-      Child Friendly:
-      <RadioGroup value={childFriendly.state.value}>
-        <Radio value={1}>Yes</Radio>
-        <Radio value={2}>No</Radio>
-      </RadioGroup>
-    </ItemDiv>
-
-    <ItemDiv>
-      Notes About Children:
-      <TextArea autosize={{ minRows: 2 }} />
-    </ItemDiv>
-
-    <ItemDiv>
-      Household currently has animals?:
-      <RadioGroup value={animalsPresent.state.value}>
-        <Radio value={1}>Yes</Radio>
-        <Radio value={2}>No</Radio>
-      </RadioGroup>
-      What type(s)?:
-      <TextArea />
-    </ItemDiv>
-
-    <ItemDiv>
-      Pets
-      <Checkbox>
-        No Pets Allowed // Make controlled checkbox that disables below choices
-      </Checkbox>
-      <Checkbox>
-        Large Dogs (more than 50 lbs.)
-      </Checkbox>
-      <Checkbox>
-        Small Dogs (less than 50 lbs.)
-      </Checkbox>
-      <Checkbox>
-        Cats
-      </Checkbox>
-      <Checkbox>
-        Horses
-      </Checkbox>
-      <Checkbox>
-        Birds
-      </Checkbox>
-      <Checkbox>
-        Reptiles
-      </Checkbox>
-      <Checkbox>
-        Rodents (i.e. Rabbits, Hamsters, Gerbils)
-      </Checkbox>
-    </ItemDiv>
-
-    <ItemDiv>
-      Notes About Pets:
-      <TextArea autosize={{ minRows: 2 }} />
-    </ItemDiv>
-
-    <ItemDiv>
-      Photo(s) of Housing:
-      <Upload {...housingPhotoListProps}>
-        <Button>
-          <Icon type="upload" /> upload
-        </Button>
-      </Upload>
-      <br />
-  </ItemDiv>
-
-    <ItemDiv>
-      Additional Information
-      <TextArea placeholder="Additional Information" autosize={{ minRows: 2 }} />
-    </ItemDiv>
-
-    <button onClick={() => actions.create(formData)}>
-      Submit!
-    </button>
   </Layout>
 )
 
